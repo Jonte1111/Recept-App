d8ac3769af9dce0fc22b4f22efc0d34a5d9e77c4
Jonathan <jonte1275@gmail.com>
2023-07-01 11:00:21 +0200

    garbage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useRef, useState } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [update, setUpdate] = useState(0);
   //Binding
-  const todoText = useRef();
   //Side effects/lifecycle
-  useEffect(() => {
-    const existingTodos = localStorage.getItem('todos');
-    setTodos(existingTodos ? JSON.parse(existingTodos) : []);
-  }, []);
-  //Events
-  function addTodo(event) {
-    event.preventDefault();
-    const next = [...todos, todoText.current.value];
-    setTodos(next);
-    localStorage.setItem('todos', JSON.stringify(next));
-  }
-  return(
-    <div>
-      <ul>
-        {todos.map(todo => (<li key={todo}>{todo}</li>))}
-      </ul>
-      <form onSubmit={addTodo}>
-        <input ref={todoText}/>
-        <input type="submit" value="Add todo"></input>
+  //Mount
+  //Update
+  //Unmount
 
-      </form>
+  //Events
+  function addIngredient(e) {
+    const data = e.data;
+    const ingredient = data.map((data) => <li>{data}</li>);
+    return <ul>{ingredient}</ul>
+  }
+    return(
+    <div>
+      <h1>
+        <p>Lägg till ingredienser!</p>
+      </h1>
+    <div id="ingredientList">
+      <ol>
+        <li>Morot</li>
+      </ol>
+    </div>
+    <form>
+      <input type="text" placeholder='fot'></input>      
+      <Dropdown>
+        <Dropdown.Toggle variant='success' id='dropdown-basic'>
+          Enhet
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item href="#action-1">Volym</Dropdown.Item>
+          <Dropdown.Item href="#action-2">Vikt</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </form>
+    <button onClick={addIngredient}>Ny Ingrediens</button>
     </div>
 
 
